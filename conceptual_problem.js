b9228f3ff6795f771dd8b2e7ae7badae26e9b531
Khan Md Yousuf <ykhanmc@gmail.com>
2023-08-07 20:10:52 +0600

    first commit

---
+++ conceptual_problem.js
@@ -0,0 +1,44 @@
+// // function isInteger(number){
+// //    if(typeof number !== "number"){
+// //     return'Please provide a number'
+// //    }
+// //    else if(number % 1 == 0){
+// //     return true;
+// //    }
+// //    else{
+// //     return false;
+// //    }
+// // }
+// // console.log(isInteger(2.5))
+
+
+// // Problem -01
+// function isInteger(number){
+// if(typeof number !== "number"){
+//     return 'Please Provide a valid Number.'
+// }else if(number % 1 == 0){
+//     return true;
+// }
+// else{
+//     return false;
+// }
+
+// }
+// console.log(isInteger(2))
+
+
+// Problem -02:
+// function isJavaScriptFile(x){
+// if(typeof x !== "string"){
+//     return 'please Provide a string.'
+// }
+// else if(x.endsWith(".js") === true){
+//     return true;
+// }
+// else{
+//     return false;
+// }
+// }
+// let x = "index.html"
+// console.log(isJavaScriptFile(x))
+
---
+++ practice.js
@@ -0,0 +1,55 @@
+// let razu = '100.02';
+// let zohan = parseFloat(razu)
+// // console.log(typeof(zohan))
+
+// // Sum to fixed:
+// let num1 = .1;
+// let num2 = .2;
+// let sum = num1 + num2;
+
+// sum = sum.toFixed(2);
+// // console.log(sum);
+
+// // Array:
+// let numbers = [45, 65, 68, 23, 53, 12, 21];
+// // console.log(numbers)
+// // console.log(numbers.length)
+// // console.log(numbers.lastIndexOf(21))
+// console.log = (numbers.indexOf(53));
+
+// Case Conversion:
+// const userName = "ali raza"
+// const userInput = "ALI REZA"
+// if (userName.toLowerCase() === userInput.toLowerCase()){
+//     console.log('valid User')
+// }
+// else{
+//     console.log('invalid user')
+// }
+
+// const userName = "rohan";
+// const inputName = "Zohan";
+// if (userName.toLowerCase() === inputName.toLowerCase()) {
+//   console.log("valid user");
+// } else {
+//   console.log("invalid user");
+// }
+
+const definition = 'we can see that. strings are immutable. while arrays are mutable'
+
+// const doesExist = definition.includes('12');
+// console.log(doesExist);
+// console.log(definition.endsWith('mutable'));
+// const parts = definition.split(' ');
+// console.log(parts);
+// const parts = definition.split('.');
+// console.log(parts);
+// const parts = definition.split('');
+// console.log(parts);
+
+// const partial = definition.slice(3, 10);
+// // console.log(partial);
+
+// const partial2 = definition.substring(3, 10);
+// console.log(partial2)
+
