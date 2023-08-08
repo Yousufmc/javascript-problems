5e871ca06a8d412d513daff76342666e3b8fb700
Khan Md Yousuf <ykhanmc@gmail.com>
2023-08-08 09:21:46 +0600

    3rd commit

--- conceptual_problem.js
+++ conceptual_problem.js
@@ -66,3 +66,47 @@
 //   }
 // }
 // isLGSeven(5);
+
+// // Intermediate Problem-01:
+// function findingBadData(arr) {
+//   if (!Array.isArray(arr)) {
+//     return "Please provide me a array";
+//   } else {
+//     let badData = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//       if (typeof arr[i] !== "number") {
+//         return "please give me number";
+//       } else {
+//         if (arr[i] < 0) {
+//           badData++;
+//         }
+//       }
+//     }
+//     return badData
+//   }
+// }
+// const arr = [-5, 2, -7, -9] ;
+// console.log(findingBadData(arr))
+// // console.log(typeof(arr));
+
+// Problem -02:
+
+// function gemsToDiamond(x, y, z){
+//   if(typeof x !== "number" || typeof y !== "number" ||typeof z !== "number"){
+//     return 'give a valid number'
+//   }
+//   else if(x < 0 || y < 0 || z < 0){
+//     return 'give a positive number.'
+//   }else{
+//     const num1 = 21;
+//     const num2 = 32;
+//     const num3 = 43;
+//     const total = 21*x + 32*y + 43*z
+//     if(total > 2000) {return total- 2000}
+//     else{return total}
+//   }
+// }
+
+// // console.log(gemsToDiamond(1, 1, 1))
+// // console.log(gemsToDiamond(20, 200, 50))
+// console.log(gemsToDiamond(100, 5, 1))
